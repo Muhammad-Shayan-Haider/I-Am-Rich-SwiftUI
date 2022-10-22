47f78e67392eb35090a058371b1a1128faf04b1f
Muhammad Shayan <mshayan@pie-int.com>
2022-10-22 12:02:16 +0500

    Initial Commit

---
+++ I Am Rich (SwiftUI)/I_Am_Rich__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich__SwiftUI_App.swift
+//  I Am Rich (SwiftUI)
+//
+//  Created by Muhammad Shayyan on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
